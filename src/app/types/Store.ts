70e1ba3f924cac076788b6a31d4377d918baf8a7
qwer0114 <skyok203@naver.com>
2023-10-11 10:42:42 +0900

    Refactor : Store type 수정

--- src/app/types/Store.ts
+++ src/app/types/Store.ts
@@ -1,5 +1,6 @@
-// 스토어 공통 타입
-export type Store = {
+// 스토어 인터페이스
+export interface BaseStore {
+  id: string;
   name: string;
   displayName?: string | null;
   address: string;
@@ -10,52 +11,49 @@ export type Store = {
     longitude: number;
   } | null;
   cateogyId: string;
-};
+}
 
 // 카테고리 기반 가게 조회
-export type CategoryStore = Store & {
-  id: string;
-};
+export type CategoryStore = BaseStore;
 
-//가게 생성
-export type NewStore = Store;
+// 가게 생성
+export type NewStore = Omit<BaseStore, "id">;
 
-// 영업시간 조회 성공 특정 요일
-export type StoreHour = {
+// 영업시간 인터페이스
+export interface BaseStoreHour {
   storeId: string;
   dayOfweek: string;
   storeHours: { openAt: string; closeAt: string }[];
-};
+}
+
+// 영업시간 특정 요일 조회 성공
+export type StoreHour = BaseStoreHour;
 
 // 영업시간 조회 성공
-export type StoreHours = StoreHour[];
+export type StoreHours = BaseStoreHour[];
 
 //영업 시간 수정
 export type UpdateStoreHours = {
   storeHours: { dayOfWeek: string; openAt: string; closeAt: string }[];
 };
 
-// 메뉴 공통 타입 추출
-export type BaseMenu = {
+// 메뉴 인터페이스
+export interface BaseMenu {
+  id: number;
   name: string;
   price: number;
-  imageUri: string | null;
-};
+  imageUri?: string | null;
+}
 
 //메뉴 조회
-export type Menu = BaseMenu & {
-  id: number;
-};
+export type Menu = BaseMenu;
 
 // 메뉴 전체 조회
 export type Menus = Menu[];
 
 //메뉴 생성
-export type NewMenu = BaseMenu;
+export type NewMenu = Omit<BaseMenu, "id">;
 
 //메뉴 수정
-export type UpdateMenu = {
-  name?: string;
-  price?: string;
-  imageUri?: string | null;
-}; // 필드 중 하나만 있어도 된다
+export type UpdateMenu = Partial<Omit<BaseMenu, "id">>; // 필드 중 하나만 있어도 된다
+
