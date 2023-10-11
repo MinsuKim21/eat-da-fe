7e9cbd18da524d7a3f1c68094bd3fb4e61ce5cf7
qwer0114 <skyok203@naver.com>
2023-10-11 10:43:48 +0900

    Refactor : Banner type 수정

--- src/app/types/Banner.ts
+++ src/app/types/Banner.ts
@@ -1,20 +1,19 @@
-// 특정 배너 조회
-type Banner = {
+export interface BaseBanner {
   id: number;
   order: number;
   imageUri: string;
   link: string;
   countOfClicks: number;
-};
+}
+
+// 특정 배너 조회
+export type Banner = BaseBanner;
 
 //배너 수정
-type UpdateBanner = {
-  order: number;
-  imageUri: string;
-  link: string;
-};
+export type UpdateBanner = Omit<BaseBanner, "id" | "countOfClicks">;
+
 // 배너 생성
-type NewBanner = UpdateBanner;
+export type NewBanner = UpdateBanner;
 
 // 전체 배너 조회
-type Banners = Banner[];
+export type Banners = BaseBanner[];
