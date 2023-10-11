92f843b20016df71113255ce8f71f24fb9786f06
qwer0114 <skyok203@naver.com>
2023-10-11 14:36:22 +0900

    feat : IconButton 컴포트 정의

---
+++ src/app/components/IconButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { ReactElement, ReactNode } from "react";
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    children: ReactNode;
+};
+
+export default function IconButton({ children, ...rest }: ButtonProps) {
+    return <button {...rest}>{children}</button>;
+}
+
