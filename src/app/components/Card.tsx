27e4b1491fe02ceb7e0b3a630416fb4b41e5ece2
MinsuKim <minsu020302@gmail.com>
2023-11-14 18:43:27 +0900

    test

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import React from 'react';
 
-export default function Card() {
+export default function Card(props: any) {
+  console.log(props);
   return (
     <div className="w-80 h-32 relative  bg-zinc-50 rounded-md mx-1">
       <div className="flex justify-start">
         <div className="p-2 relative">
-          <Image className="w-24 rounded-xl bg-zinc-500 aspect-square" src={'/assets/Untitled.png'} alt="test" width={100} height={100} />
+          <Image className="w-24 rounded-xl bg-zinc-500 aspect-square" src={props.imgSrc} alt="test" width={100} height={100} />
         </div>
         <div className="flex flex-col items-start">
           <p className=" font-bold text-lg">음식점이름</p>
