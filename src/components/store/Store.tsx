cf2bc101b69cdd068e214b75346d89242fa3eae5
qwer0114 <skyok203@naver.com>
2023-11-22 21:36:00 +0900

    feat : Store 컴포넌트 UI 정의

--- src/components/store/Store.tsx
+++ src/components/store/Store.tsx
@@ -1,22 +1,26 @@
-import React, { ForwardedRef } from 'react';
-import StoreImage from './StoreImage';
-
-type StoreProp = {
-  result: any;
-};
+import React from "react";
+import StoreImage from "./StoreImage";
+import { ReactComponent as PackageIcon } from "../../assets/packageIcon.svg";
+import { ReactComponent as EyeIcon } from "../../assets/eye.svg";
 
 export default function Store() {
   return (
-    <div className="w-80 h-32 bg-white rounded-md relative flex justify-start items-center">
-      <div className="p-2">
-        <StoreImage imgSrc={""} alt={"Store"} />
-      </div>
-      <div className="self-start pt-3 pl-2">
-        <p className="text-xl font-bold pb-1">음식점 이름</p>
-        <p className="text-sm text-zinc-700">대표 메뉴명</p>
-        <p className="text-sm">02-1234-1234</p>
+    <div className="w-80 h-32 bg-white rounded-md flex">
+      <div className="w-full flex justify-start items-center">
+        <div className="p-2 relative">
+          <StoreImage imgSrc={""} alt={"Store"} />
+          <div className="w-20 h-4 bg-stone-950 rounded-2xl flex items-center absolute bottom-4 left-4">
+            <EyeIcon className="mx-1" />
+            <p className=" text-white text-[9px] leading-3 ">50명 조회중</p>
+          </div>
+        </div>
+        <div className="self-start pt-3 pl-2 flex flex-col">
+          <p className="text-xl font-bold pb-1">음식점 이름</p>
+          <p className="text-sm text-zinc-700">대표 메뉴명</p>
+          <p className="text-sm text-zinc-700">02-1234-1234</p>
+        </div>
+        <PackageIcon className="self-end my-3 ml-auto mr-3" />
       </div>
     </div>
   );
 }
-
