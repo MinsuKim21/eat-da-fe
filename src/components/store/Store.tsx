ecfca7277f68a87d051502614f732ec7bf1c2b64
qwer0114 <skyok203@naver.com>
2023-11-22 16:46:27 +0900

    feat: Store UI 정

--- src/components/store/Store.tsx
+++ src/components/store/Store.tsx
@@ -1,42 +1,22 @@
 import React, { ForwardedRef } from 'react';
-import IconButton from '../IconButton';
-import Icon1 from '../Icon';
-import location from '@../../public/assets/location.svg';
-import call from '@../../public/assets/call.svg';
-import eye from '@../../public/assets/eye.svg';
-import withList from '../../hocs/withList';
+import StoreImage from './StoreImage';
 
 type StoreProp = {
   result: any;
 };
 
-function Store({ result }: StoreProp) {
+export default function Store() {
   return (
-    <div>
-      <div className="w-full max-w-md h-[120px] bg-zinc-700 rounded-lg flex justify-between pt-[15px] px-[15px]">
-        <div>
-          <div className="text-[15px] font-bold font-['Inter'] leading-[18px] mb-[2px]">음식점 이름</div>
-          <div className=" text-[10px] font-medium font-['Inter'] leading-[14px] mb-[28px]">주소</div>
-          <div className=" text-xs font-semibold font-['Inter'] mb-[4px]">영업일/시간</div>
-          <div className="w-20 h-4 bg-stone-950 rounded-2xl flex justify-evenly items-center">
-            <Icon1 iconName={eye} alt="조회 이미지"></Icon1>
-            <div className=" text-white text-[9px] font-normal font-['Inter'] leading-3 ">50명 조회중</div>
-          </div>
-        </div>
-        <div>
-          <IconButton className="w-10 h-10 bg-neutral-950 rounded-full flex items-center justify-center mb-[10px]">
-            <Icon1 iconName={location} alt="위치 이미지"></Icon1>
-          </IconButton>
-          <IconButton className="w-10 h-10 bg-neutral-950 rounded-full flex items-center justify-center">
-            <Icon1 iconName={call} alt="전화 이미지"></Icon1>
-          </IconButton>
-        </div>
+    <div className="w-80 h-32 bg-white rounded-md relative flex justify-start items-center">
+      <div className="p-2">
+        <StoreImage imgSrc={""} alt={"Store"} />
       </div>
-      <div className="w-[344px] h-10 bg-black rounded-[9px] flex justify-center items-center">
-        <IconButton className="w-[162px] h-5 text-white text-[17px] font-extrabold font-['Inter'] leading-snug">MENU</IconButton>
+      <div className="self-start pt-3 pl-2">
+        <p className="text-xl font-bold pb-1">음식점 이름</p>
+        <p className="text-sm text-zinc-700">대표 메뉴명</p>
+        <p className="text-sm">02-1234-1234</p>
       </div>
     </div>
   );
 }
 
-export default withList(Store, () => {}); // FIXME:
