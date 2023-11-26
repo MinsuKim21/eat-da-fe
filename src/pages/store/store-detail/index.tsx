b36e40beb266b1cac2d4771c687510f12c74dda4
qwer0114 <skyok203@naver.com>
2023-11-26 14:00:10 +0900

    feat : Storepage 연결

--- src/pages/store/store-detail/index.tsx
+++ src/pages/store/store-detail/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useLocation } from 'react-router';
-import Icon from '../../../components/Icon';
-import location from '../../../assets/location.svg';
-import call from '../../../assets/call.svg';
-import eye from '../../../assets/eye.svg';
+import React from "react";
+import { useLocation } from "react-router";
 // import withList from '../../hocs/withList';
-import IconButton from '../../../components/IconButton';
+import Store from "../../../components/store/Store";
 
 export default function StorePage() {
   const { pathname } = useLocation();
-  const id = pathname.split('/')[2];
-  return <div className=''>{id}</div>;
+  const id = pathname.split("/")[2];
+  return (
+    <main className="w-full h-full flex flex-col gap-y-[0.5rem] bg-black">
+      <Store id={id}></Store>
+    </main>
+  );
 }
