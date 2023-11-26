d1314932835db428d0bd556629a4404e443f547f
qwer0114 <skyok203@naver.com>
2023-11-26 13:59:04 +0900

    feat : Store UI 정의

--- src/components/store/Store.tsx
+++ src/components/store/Store.tsx
@@ -1,25 +1,30 @@
 import React from "react";
-import StoreImage from "./StoreImage";
-import { ReactComponent as PackageIcon } from "../../assets/packageIcon.svg";
+import { ReactComponent as CallIcon } from "../../assets/call.svg";
+import { ReactComponent as LocationIcon } from "../../assets/location.svg";
 import { ReactComponent as EyeIcon } from "../../assets/eye.svg";
-
-export default function Store() {
+import StoreImage from "./StoreImage";
+import Menu from "./Menu";
+export default function Store({ id }) {
   return (
-    <div className="w-80 h-32 bg-white rounded-md flex">
-      <div className="w-full flex justify-start items-center">
-        <div className="p-2 relative">
-          <StoreImage imgSrc={""} alt={"Store"} />
-          <div className="w-20 h-4 bg-stone-950 rounded-2xl flex items-center absolute bottom-4 left-4">
-            <EyeIcon className="mx-1" />
-            <p className=" text-white text-[9px] leading-3 ">50명 조회중</p>
-          </div>
+    <div className="w-full max-w-md  bg-white rounded-md">
+      <StoreImage imgSrc={""}>
+        <CallIcon />
+        <LocationIcon />
+        <EyeIcon />
+      </StoreImage>
+      <div className="p-5">
+        <div className="text-base font-bold font-['Inter'] leading-snug mb-5">
+          음식점 이름
         </div>
-        <div className="self-start pt-3 pl-2 flex flex-col">
-          <p className="text-xl font-bold pb-1">음식점 이름</p>
-          <p className="text-sm text-zinc-700">대표 메뉴명</p>
-          <p className="text-sm text-zinc-700">02-1234-1234</p>
+        <div className=" text-red-500 text-2xl font-semibold font-['Inter'] leading-snug">
+          MENU
         </div>
-        <PackageIcon className="self-end my-3 ml-auto mr-3" />
+        <Menu></Menu>
+        <Menu></Menu>
+        <Menu></Menu>
+        <Menu></Menu>
+        <Menu></Menu>
+        <Menu></Menu>
       </div>
     </div>
   );
